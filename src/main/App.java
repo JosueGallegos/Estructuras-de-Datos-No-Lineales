d7c248988ee655ff9da03563737b0c5eb70d656f
JosueGallegos <jgallegoss@est.ups.edu.ec>
2024-06-19 21:38:42 -0500

    Clases Arboles

---
+++ src/main/App.java
@@ -0,0 +1,35 @@
+package main;
+import main.Materia.Controllers.ArbolBinario;
+import main.Materia.Models.Node;
+/* 
+ *
+ * 
+*/
+public class App {
+    public static void main(String[] args) throws Exception {
+        ArbolBinario arbolBinario = new ArbolBinario();
+        Node nodeRoot =new Node(1);
+        nodeRoot.setLeft(new Node (2));
+        nodeRoot.setRight(new Node (3));
+
+        nodeRoot.getLeft().setLeft(new Node (4));
+        nodeRoot.getLeft().setRight(new Node (5));
+
+        nodeRoot.getRight().setRight(new Node(6));
+        nodeRoot.getLeft().getLeft().setLeft(new Node (7));
+        nodeRoot.getLeft().getRight().setLeft(new Node(8));
+        
+        
+        arbolBinario.preOrderLoop(nodeRoot);
+        System.out.println();
+        arbolBinario.preOrderRecursivo(nodeRoot);
+        System.out.println();
+        arbolBinario.postOrdenLoop(nodeRoot);
+        System.out.println("PosOrden");
+
+
+        
+    }
+
+
+}
